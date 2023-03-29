d3666bd0adb7c5c83a8fd28b7e60bc5c86f482a5
Jack Daly <dalyj2@middleton.school.nz>
2023-03-30 11:40:53 +1300

    Update 01_yes_no_v3.py

--- 01_yes_no_v3.py
+++ 01_yes_no_v3.py
@@ -1,17 +1,23 @@
-# Ask user if they have played before
-show_instructions = input("Have you played this game before? Enter 'yes' or 'no': ").lower()
-
-# If yes, print 'program continues'
-if show_instructions == "yes" or "y":
-    print("Program continues")
-
-# If no, print 'Display instructions'
-elif show_instructions == "no" or "n":
-    print("Display instructions")
-
-# Else, show error
-else:
-    print("Please answer 'Yes' or 'No'")
+#Puts the code into a loop to make testing easier and more efficient.
 
 
-print(f"You entered '{show_instructions}'")
+show_instructions = ""
+while show_instructions != "x":
+
+    # Ask user if they have played before
+    show_instructions = input("Have you played this game before? Enter 'yes' or 'no': ").lower()
+
+    # If yes, print 'program continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("Program continues")
+
+    # If no, print 'Display instructions'
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Display instructions")
+
+    # Else, show error
+    else:
+        print("Please answer 'Yes' or 'No'")
+
+
+    print(f"You entered '{show_instructions}'")
